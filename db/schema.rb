f0b8732246dac016d86e69377313e8a9449a0f45
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-12 21:21:45 +0800

    Add default value as false to completed in task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_130645) do
+ActiveRecord::Schema.define(version: 2020_05_12_131806) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed", default: false
   end
 
 end
