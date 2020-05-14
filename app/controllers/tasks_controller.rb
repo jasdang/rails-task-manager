923b4f4abcac149ecd1a07cd6d3612f017452023
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-14 20:29:08 +0800

    add task: done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,19 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @new_task = Task.new
+  end
+
+  def create
+    Task.create(task_info)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_info
+    params.require(:task).permit(:title, :details)
+  end
 end
